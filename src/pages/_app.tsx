2bc0ecfad368a38a5e30adf2f85a29dfa29f904f
jusbuckingham <jus.buckingham@gmail.com>
2024-08-08 12:39:59 -0700

    troublshooting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import JusibleLoader from '../components/JusibleLoader';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <JusibleLoader />
       <Component {...pageProps} />
     </>
   );
