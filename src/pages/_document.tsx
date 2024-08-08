98354bfee4ecd0b138849d5eedbc537fe1e77360
jusbuckingham <jus.buckingham@gmail.com>
2024-08-08 12:43:37 -0700

    troublshooting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,10 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-          <script src="https://jusible.vercel.app/jusible.js" defer></script>
+          <script
+            src="https://jusible.vercel.app/jusible.js"
+            defer
+          />
         </Head>
         <body>
           <Main />
